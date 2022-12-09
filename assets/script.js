1c1dc6bf6782d6f84f553a107060f231ccdfdc8a
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2022-12-09 12:01:45 +1100

    added comments to script

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,52 @@
+// Define variables to handle sections and elements
+
+// Variables for High Scores button and Timer element
 var highScores = document.querySelector("#high-scores")
 var timer = document.querySelector("#timer")
 
+// Variables to handle Start Page container and elements within
 var startPage = document.querySelector("#start-page");
 var instructions = document.querySelector("#instructions");
 var start = document.querySelector("#start-button");
 
+// Variable to grab Button area for event listener delegation
 var btnArea = document.querySelector("#btnArea");
 
+// Variables for quiz page container and questions and answer elements
 var quizPage = document.querySelector("#quiz");
 var questions = document.querySelector("#questions");
-
 var optionButtonA = document.querySelector("#option-A");
 var optionButtonB = document.querySelector("#option-B");
 var optionButtonC = document.querySelector("#option-C");
 var optionButtonD = document.querySelector("#option-D");
 
+// Variables to handle Answer and Results elements
 var showAnswer = document.querySelector("#answer");
-
 var result = document.querySelector("#result");
 
+// Variables to handle final submit page and all elements within
 var submitBtn = document.querySelector("#submit");
 var playAgain = document.querySelector("#play-again");
 var clearHighScores = document.querySelector("#clear-scores");
 var initialEl = document.querySelector("#initials");
-
 var submitPage = document.querySelector("#submit-page");
 var clearBtn = document.querySelector("#clear-scores");
 var scoreList = document.querySelector("#hi-scorers");
-
 var playAgainBtn = document.querySelector("#play-again");
-
 var pastHighScores = document.querySelector("#high-scores-list");
 
+// Variables to create timer function
 var timeEl = document.querySelector(".timer");
 let timerInterval;
 var secondsLeft = 75;
 
+// Variable to set question index start point
 var quizQuestionIndex = 0;
 
+// Variable to pull saved scores from local storage or set empty array
 var savedScores = JSON.parse(localStorage.getItem("scoreInitials")) || [];
 
+// Variable to set all questions, options and answers in an array
 var allQuestions = [
     {
         question: "1 - Commonly used data types DO NOT include:",
@@ -68,17 +75,12 @@ var allQuestions = [
     }
 ];
 
+// Event listener to call startGame function
 start.addEventListener("click", function() {
     startGame();
 });
 
-function setTime() {
-    timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
-}, 1000);
-}
-
+// startGame function hides startPage, displays question area, starts timer and calls renderQuestion function
 function startGame () {
     startPage.style.display ="none";
     quizPage.style.display = "grid";
@@ -88,6 +90,15 @@ function startGame () {
     renderQuestion();
 }
 
+// Function to countdown from secondsLeft variable in increments of 1000 milliseconds
+function setTime() {
+    timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+}, 1000);
+}
+
+// Function to refer to current questions index and create question and answer from array
 function renderQuestion() {
     questions.textContent = allQuestions[quizQuestionIndex].question;
     optionButtonA.textContent = allQuestions[quizQuestionIndex].options[0];
@@ -96,7 +107,9 @@ function renderQuestion() {
     optionButtonD.textContent = allQuestions[quizQuestionIndex].options[3];
 }
 
+// Event listener to register users answer and check against correct answer in array
 btnArea.addEventListener("click", function(event){
+// If statements to notify of correct answer, notify of wrong answer and decrement 10seconds
     if(event.target.textContent === allQuestions[quizQuestionIndex].answer){
         displayAnswer()
     } else if (event.target.textContent !== allQuestions[quizQuestionIndex].answer){
@@ -104,7 +117,9 @@ btnArea.addEventListener("click", function(event){
             showAnswer.style.display = "block";
             result.textContent = "Wrong! Minus 10 Seconds";
     } 
+    // Adds 1 to index to move to next question in array
     quizQuestionIndex++;
+    // If statement to end quiz when index reaches final question
     if(quizQuestionIndex >= 5)
     {
         displaySaveScore();
@@ -113,11 +128,13 @@ btnArea.addEventListener("click", function(event){
     renderQuestion();
 });
 
+// Function to display correct answer notification and answer from array
 function displayAnswer(){
     showAnswer.style.display = "block";
     result.textContent = "Correct! " + allQuestions[quizQuestionIndex].answer;
 };
 
+// Function to end quiz timer, hide elements from quiz page and display final score and submit page
 function displaySaveScore() {
     clearInterval(timerInterval);
     timer.style.display = "none";
@@ -128,26 +145,35 @@ function displaySaveScore() {
     pastHighScores.textContent = "Congratulations you got " + secondsLeft + "!";
 };
 
+// Event listener to submit initials, call saveScore function
 submitBtn.addEventListener("click", saveScore);
 
+// Function to detect if input has been entered, bring up alert if no value
 function saveScore(event){
     event.preventDefault();
     if(!initialEl.value){
         alert("Please enter your initials");
         return;
     }
+    // Set variable for local storage objects
     var scoreInitials = {
         initials: initialEl.value,
         score: secondsLeft
     };
+    // Push saved objects to savedScores array
     savedScores.push(scoreInitials)
+    // Save initials and score in local storage
     localStorage.setItem("scoreInitials", JSON.stringify(savedScores));
+    // Call renderScore function
     renderScore()
+    // Disable submit button once value entered
     document.getElementById("submit").disabled = true;
 };
 
+// Function to get objects from local storage
 function renderScore(){
     var lastScore = JSON.parse(localStorage.getItem("scoreInitials"))
+    // Loop to run through savedScores array and print to screen
     for (i=0; i < lastScore.length; i++){
         var hiScorers = document.querySelector("#hi-scorers");
         var scorer = document.createElement("li");
@@ -156,6 +182,7 @@ function renderScore(){
     }
 };
 
+// Event listener to show saved high scores
 highScores.addEventListener("click", function(event) {
     startPage.style.display = "none";
     clearBtn.style.display = "none";
@@ -166,6 +193,7 @@ highScores.addEventListener("click", function(event) {
     renderScore();
 });
 
+// Event listener to clear local storage and empty savedScores array when button pressed
 clearBtn.addEventListener("click", function(event) {
     event.preventDefault();
     localStorage.clear();
@@ -173,6 +201,7 @@ clearBtn.addEventListener("click", function(event) {
     scoreList.style.display = "none";
 });
 
+// Event listener to activate page reload to go back to the start of the quiz
 playAgainBtn.addEventListener("click", function(event) {
     event.preventDefault();
     window.location.reload();
